51ba6544587140169d0876b28d3ed04b1cb1e5c9
maeterri <sebastiterr@frba.utn.edu.ar>
2023-05-02 19:10:39 -0300

    Empiezo con el tema de la base de datos, espero no crashee

--- app/src/main/java/com/utn/booklist/entities/Book.kt
+++ app/src/main/java/com/utn/booklist/entities/Book.kt
@@ -1,11 +1,40 @@
 package com.utn.booklist.entities
 
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "books")
 class Book (
-    var id : Int,
-    var title : String,
-    var author : String,
-    var year : Int)
+     id : Int,
+     title : String,
+     author : String,
+     description : String,
+     image : String,
+     year : Int)
 
 {
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "id")
+    var id : Int
+    @ColumnInfo(name = "title")
+    var title : String
+    @ColumnInfo(name = "author")
+    var author : String
+    @ColumnInfo(name = "description")
+    var description : String
+    @ColumnInfo(name = "image")
+    var image : String
+    @ColumnInfo(name = "year")
+    var year : Int
+
+init {
+    this.id= id
+     this.title = title
+     this.author= author
+     this.description= description
+      this.image= image
+     this.year= year
+}
 
 }
