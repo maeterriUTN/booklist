e028e6abf6eacf390e644464ee78a2eab64a50c4
Marcelo Terrizzano <sebastiterr@frba.utn.edu.ar>
2023-05-16 10:48:40 -0300

    Subida con todas las tareas andando

--- app/src/main/java/com/utn/booklist/activities/SplashActivity.kt
+++ app/src/main/java/com/utn/booklist/activities/SplashActivity.kt
@@ -3,13 +3,21 @@ package com.utn.booklist.activities
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import com.utn.booklist.R
 
 class SplashActivity : AppCompatActivity() {
+    private val SPLASH_TIME_OUT:Long = 3000 // 3 sec
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+        Handler().postDelayed(
+
+            {
         var intent = Intent(this, LoginActivity::class.java)
         startActivity(intent)
+                finish()
+            }
+            , SPLASH_TIME_OUT)
     }
 }
